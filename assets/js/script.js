907c27e5aeaa34602d8cf7c617180b3f0706ae0f
Mahmoud Ammar <80252748+MahmoudAziz-Ammar@users.noreply.github.com>
2025-11-16 15:05:03 +0100

    2sc commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["data science", "machine learning", "Natural Language Processing", "Artificial Intelligence","web development"],
+    strings: ["Data  science", "Data Engineering", "Natural Language Processing", "Artificial Intelligence","web development"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
